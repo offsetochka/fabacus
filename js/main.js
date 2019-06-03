ab5c2ae379ecf027dbd6f4f25708929b50f87c51
Georgy <georgsby@gmail.com>
2019-06-04 00:28:32 +0300

    asd

--- js/main.js
+++ js/main.js
@@ -292,7 +292,7 @@ $(document).ready(function () {
 
     $(".lazy").recliner({
         attrib: "data-src", // selector for attribute containing the media src
-        throttle: 300,      // millisecond interval at which to process events
+        throttle: 100,      // millisecond interval at which to process events
         threshold: 100,     // scroll distance from element before its loaded
         printable: true,    // be printer friendly and show all elements on document print
         live: true          // auto bind lazy loading to ajax loaded elements
